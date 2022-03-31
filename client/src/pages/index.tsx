291b5ebc711aeeeaa33357384735c9677d3bfe49
Mihir Patel <mihirkpatel9702@gmail.com>
2022-03-30 23:37:03 -0400

    ui layout setup perfect 4 col grid

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="grid grid-cols-10 h-screen w-full">
+      <div className="bg-gray-200 text-center text-2xl">
+        <h2>Servers</h2>
+      </div>
+      <div className="bg-gray-300 col-span-2 text-center text-2xl">
+        <h2>Channels</h2>
+      </div>
+      <div className="bg-gray-400 col-span-5 text-center text-2xl">
+        <h2>Messages</h2>
+      </div>
+      <div className="bg-gray-500 col-span-2 text-center text-2xl">
+        <h2>Users</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
